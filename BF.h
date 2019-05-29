bdbfc2d53d5cd93ff402a76f01d871dcd4eb8208
ubuntu <218370@student.pwr.edu.pl>
2019-05-29 06:32:24 +0200

    Dodano brakujące nagłówki

--- BF.h
+++ BF.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdlib>
 #include <algorithm>
+#include <climits>
 #include "Graph.h"
 
 class BF {
--- Graph.h
+++ Graph.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <climits>
 
 using namespace std;
 
